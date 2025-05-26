a6d115d556ca21ee55cdf5dde54d7ea62842fc95
Zak0716 <b.sakthi196@gmail.com>
2025-05-26 20:25:27 +0530

    3rd commit.java
    adding static method

--- Sample.java
+++ Sample.java
@@ -1,12 +1,16 @@
 public class Sample
 {
 
+  public static void add()
+  {
+    int a=5, b=10, c;
+    c = a + b;
+    System.out.println(c);
+  }
 
 public static void main(String[] args)
 { 
-  int a=5, b=10, c;
-  c = a + b;
-  System.out.println(c);
+    add();
 }
   
 }
